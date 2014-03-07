ff985f6c67349a69427848145b07a77b6ffb237c
Jens Jansson <peppe@novacode.fi>
2014-03-07 20:00:49 +0200

    fixed filter to serve static files correctly.

--- src/main/java/com/jensjansson/forwardfilter/ForwardFilter.java
+++ src/main/java/com/jensjansson/forwardfilter/ForwardFilter.java
@@ -14,10 +14,21 @@ public class ForwardFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		if (((HttpServletRequest) request).getRequestURI().contains("/page.html")) {
-			System.out.println("page.html was requested. We should serve that page.");
+		HttpServletRequest req = (HttpServletRequest) request;
+		String path = req.getRequestURI().substring(
+				req.getContextPath().length());
+
+		if (path.startsWith("/page.html")) {
+			chain.doFilter(request, response); // Goes to default servlet.
+		} else if (path.startsWith("/VAADIN/")) {
+			chain.doFilter(request, response); // Goes to default servlet.
+		} else if (path.startsWith("/UIDL/")) {
+			request.getRequestDispatcher("/app/UIDL/").forward(request,
+					response); // UIDL calls within Vaadin app
+		} else {
+			// Vaadin app calls
+			request.getRequestDispatcher("/app").forward(request, response);
 		}
-		chain.doFilter(request, response);
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
